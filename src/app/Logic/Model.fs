ca829fe03049d26f22dce506495ef42ee481608f
ClementLegouest <clement.legouest@gmail.com>
2020-01-08 23:10:05 +0100

    [evo] Logic/Model adding deny and cancel

--- src/app/Logic/Model.fs
+++ src/app/Logic/Model.fs
@@ -6,21 +6,29 @@ open System
 type Command =
     | RequestTimeOff of TimeOffRequest
     | ValidateRequest of UserId * Guid
+    | DenyRequest of UserId * Guid
+    | CancelRequest of UserId * Guid
     with
     member this.UserId =
         match this with
         | RequestTimeOff request -> request.UserId
         | ValidateRequest (userId, _) -> userId
+        | DenyRequest (userId, _) -> userId
+        | CancelRequest (userId, _) -> userId
 
 // And our events
 type RequestEvent =
     | RequestCreated of TimeOffRequest
     | RequestValidated of TimeOffRequest
+    | RequestDenied of TimeOffRequest
+    | RequestCanceled of TimeOffRequest
     with
     member this.Request =
         match this with
         | RequestCreated request -> request
         | RequestValidated request -> request
+        | RequestDenied request -> request
+        | RequestCanceled request -> request
 
 // We then define the state of the system,
 // and our 2 main functions `decide` and `evolve`
@@ -29,17 +37,23 @@ module Logic =
     type RequestState =
         | NotCreated
         | PendingValidation of TimeOffRequest
-        | Validated of TimeOffRequest with
+        | Validated of TimeOffRequest
+        | Denied of TimeOffRequest
+        | Canceled of TimeOffRequest with
         member this.Request =
             match this with
             | NotCreated -> invalidOp "Not created"
             | PendingValidation request
             | Validated request -> request
+            | Denied request -> request
+            | Canceled request -> request
         member this.IsActive =
             match this with
             | NotCreated -> false
             | PendingValidation _
             | Validated _ -> true
+            | Denied _ -> false
+            | Canceled _ -> false
 
     type UserRequestsState = Map<Guid, RequestState>
 
@@ -47,6 +61,8 @@ module Logic =
         match event with
         | RequestCreated request -> PendingValidation request
         | RequestValidated request -> Validated request
+        | RequestDenied request -> Denied request
+        | RequestCanceled request -> Canceled request
 
     let evolveUserRequests (userRequests: UserRequestsState) (event: RequestEvent) =
         let requestState = defaultArg (Map.tryFind event.Request.RequestId userRequests) NotCreated
@@ -78,6 +94,20 @@ module Logic =
         | _ ->
             Error "Request cannot be validated"
 
+    let denyRequest requestState =
+        match requestState with
+        | PendingValidation request ->
+            Ok [RequestDenied request]
+        | _ ->
+            Error "Request cannot be denied"
+
+    let cancelRequest requestState =
+        match requestState with
+        | PendingValidation request ->
+            Ok [RequestCanceled request]
+        | _ ->
+            Error "Request cannot be canceled"
+
     let decide (userRequests: UserRequestsState) (user: User) (command: Command) =
         let relatedUserId = command.UserId
         match user with
@@ -101,3 +131,17 @@ module Logic =
                 else
                     let requestState = defaultArg (userRequests.TryFind requestId) NotCreated
                     validateRequest requestState
+
+            | DenyRequest (_, requestId) ->
+                if user <> Manager then
+                    Error "Unauthorized"
+                else
+                    let requestState = defaultArg (userRequests.TryFind requestId) NotCreated
+                    denyRequest requestState
+
+            | CancelRequest (_, requestId) ->
+                if user <> Manager then
+                    Error "Unauthorized"
+                else
+                    let requestState = defaultArg (userRequests.TryFind requestId) NotCreated
+                    cancelRequest requestState
